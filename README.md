7524fb75108ead94294d0d84148912545007f46d
Yuriy Rudenko <yurmag@mail.ru>
2019-07-26 22:37:10 +0300

    the first commit

---
+++ README.md
@@ -0,0 +1,126 @@
+# iperf3-imix-test
+
+This is an attempt to test you network with "IMIX profile" using open-source software.
+
+## Prerequisites
+ - iptables need to be adjusted on both iPerf server and iPerf client
+ - passwordless access to be configured using RSA keys
+ - basic understanding of bash
+ - bc package must be installed on client's side
+
+## Output sample
+```
+[root@centos6 ~]# ./udp-iperf.sh 192.168.136.4 2
+iPerf3 will run 3 flows of IMIX traffic for the duration of 10 seconds:
+ 64-byte packets   (18 bytes of payload)   with configured bandwidth of 380,000 bits/s
+ 576-byte packets  (530 bytes of payload)  with configured bandwidth of 1,000,000 bits/s
+ 1350-byte packets (1304 bytes of payload) with configured bandwidth of 620,000 bits/s
+
+iPerf3 was running for 10 seconds with bandwidth limit set to 2Mb/s.
+
+Data                 Sent       Received       Lost        Drop     Bandwidth     Bandwidth
+flow              packets        packets    packets        rate      (sender)    (receiver)
+64-byte              5512           5345        167       3.00%      .370Mb/s      .359Mb/s
+576-byte             2344           2344          0       0.00%     1.117Mb/s     1.117Mb/s
+1350-byte             600             72        528      88.00%      .625Mb/s      .075Mb/s
+
+Summary              8456           7761        695       8.00%     2.112Mb/s     1.551Mb/s
+```
+
+## IMIX traffic profile
+IMIX stands for Internet Mix prepresenting traffic of various nature/size. I was unable to find any standards that identify the exact size of IMIX traffic pattern except a wiki page, which is not a standard ;)
+
+It is a know fact, that the minimum Ethernet frame payload is 46 bytes and the maximum Ethernet frame payload is 1500 bytes for regular/low level hardware network. RFC879/RFC6691 declare minimum size for IPv4 datagram is 576 bytes. Than being said, IMIX traffic pattern could be represented in three values: 64 bytes, 576 bytes and 1500 bytes.
+
+Wiki page for IMIX (https://en.wikipedia.org/wiki/Internet_Mix) and Cisco in its own testing tool (https://github.com/cisco-system-traffic-generator/trex-core/blob/master/scripts/stl/imix.py) use the following traffic distribution:
+
+|IP payload length|Total Ethernet length|Number of packets in IMIX stream|Percent of packets|Percent of traffic consumption|
+|:-----:|:-----:|:-----:|:-----:|:-----:|
+|46|64|7|58|19|
+|576|594|4|33|50|
+|1500|1518|1|9|31|
+
+
+If you use IPSec, then the maximum packet size could be less then 1400. To be safe in terms of MTU, you can use the following values
+
+|UDP payload length|IP payload length|Total Ethernet length|Percent of traffic consumption|
+|:-----:|:-----:|:-----:|:-----:|
+|64|92|110|19|
+|530|558|576|50|
+|1304|1332|1350|31|
+
+## Statistics
+Many people on the Internet complained about iPerf3 statistics provided for UDP and I am one of them. It is not accurate at all, so I used iptables with user-defined chains to count the number of packet sent/received. 
+
+## Ingress counters (server side)
+The number of received packets can be tracked with following iptables configuration:
+```
+#user-defined chain
+:IPERF_IN - [0:0]
+
+#accept control traffic
+-I INPUT -p tcp --dport 5001:5003 -j ACCEPT
+
+#iPerf3 forwarding to IPERF_IN chain
+-I INPUT -p udp --dport 5001:5003 -j IPERF_IN
+
+#Rules for specific ports
+-A IPERF_IN -p udp --dport 5001 -j ACCEPT
+-A IPERF_IN -p udp --dport 5002 -j ACCEPT
+-A IPERF_IN -p udp --dport 5003 -j ACCEPT
+```
+
+Statistics can be retrieved with the following command:
+```
+sudo iptables -L IPERF_IN -v -n -x
+```
+
+To reset the counters, use the following command:
+```
+sudo iptables -Z IPERF_IN
+```
+
+##Egress counters (client side)
+The number of sent packets can be tracked with following iptables configuration:
+```
+#user-defined chain
+:IPERF_OUT - [0:0]
+
+#iPerf3 forwarding to IPERF_OUT chain
+-A OUTPUT -p udp --dport 5000:5003 -j IPERF_OUT
+
+#Rules for specific ports
+-A IPERF_OUT -p udp --dport 5001 -j ACCEPT
+-A IPERF_OUT -p udp --dport 5002 -j ACCEPT
+-A IPERF_OUT -p udp --dport 5003 -j ACCEPT
+```
+
+Sumilar commands to show and resetting counters:
+```
+sudo iptables -L IPERF_OUT -v -n -x
+```
+
+```
+sudo iptables -Z IPERF_OUT
+```
+
+## IPERF ON THE SERVER SIDE
+iPerf3 could be run on the server side in daemon mode with following arguments utilizing different CPU cores:
+```
+iperf3 -p 5001 -s -D -A 0
+iperf3 -p 5002 -s -D -A 2
+iperf3 -p 5003 -s -D -A 4
+```
+If you do not have that many cores, then avoid -A option
+
+
+## IPERF ON THE CLIENT SIDE
+Script uses RSA key to send remote commands to the server (clear iptables counters), so RSA key and username are needed. You can specify these in lines 4 and 5 of the script.
+
+If your CPU has less than 4 cores, then remove affinity parameter (-A) and its argument in lines:
+ - 95, 96, 97
+ - 99, 100, 101
+
+That is all. Once it is done, you can run the script.
+
+BTW, log file is written for your convenience. It has a prefix of "iperf_<TIMESTAMP>".
---
+++ udp-iperf.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+
+# a path to the rsa key and username to access the remote server
+RSA_KEY="/root/.ssh/id_rsa"
+USERNAME="root"
+
+# if the number of args is not 3
+if [ $# -lt 2 ]; then
+	echo "Usage: $(basename "$0") <server_ip> BANDWIDTH [TIME]"
+	exit
+fi
+
+# RE is the regexp for IPv4 address
+# first part is non-zero number that is less than 255 followed by dot
+RE='^(([1-9])|([1-9][0-9])|1?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))\.'
+# next is any number from 0 to 255 followed by dot (two times)
+RE+='((1?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))\.){2}'
+# last is any number from 0 to 255 followed by dot 
+RE+='(1?[0-9]{1,2}|2([0-4][0-9]|5[0-5]))$'
+
+if [[ ! $1 =~ $RE ]]; then
+	echo "IP address is invalid! Usage: $(basename "$0") <server_ip> BANDWIDTH [TIME]"
+	exit
+fi
+
+ping -c1 -q $1 &>/dev/null
+STATUS=$( echo $? )
+if [[ ! $STATUS == 0 ]] ; then
+	echo "Server is unchearcable!"
+	exit
+fi
+
+if [ $# = 3 ]; then
+	TIME=$3
+else
+	TIME="10"
+fi
+
+SERVER_IP=$1
+
+# Bandwidth for traffic with is composed from:
+# user specified BW(arg[2]) is interpreted as value in Megabits.
+# This value must be multiplied by 1 000 000 (conversion to bits) and
+# devided by 8, since BW limitation is applied per flow and iPerf is
+# run with 8 flows. Effectively these two operations can be combined 
+# into one multiplication by 125 000.
+# Multiplication factor for each packet size is based on the amount 
+# of BW is expected to be used by each traffic pattern:
+#  - 64-byte the factor is 0.19 (19 percent of all traffic)
+#  - 576-byte the factor is 0.5 (50 percent of all traffic)
+#  - 1350-byte the factor is 0.31 (31 percent of all traffic)
+
+# if you use multuplication factor of 0.19 for small packets,
+# and calculate effective bandwidth, it will not match. 
+# multiplication factor of 0.04 was found empirically. this 
+# value gives the most accurate traffic pattern for small packets
+
+BW_64=$(printf "%0.f" $( echo "$2 * 125000 * 0.04" | bc ) )
+BW_576=$(printf "%0.f" $( echo "$2 * 125000 * 0.50" | bc ) )
+BW_1350=$(printf "%0.f" $( echo "$2 * 125000 * 0.31" | bc ) )
+
+# the following variables are used to indicate the expected level of
+# bandwidth utilization
+BW_64_REF=$(printf "%0.f" $( echo "$2 * 1000000 * 0.19" | bc ) )
+BW_576_REF=$(printf "%0.f" $( echo "$2 * 1000000 * 0.50" | bc ) )
+BW_1350_REF=$(printf "%0.f" $( echo "$2 * 1000000 * 0.31" | bc ) )
+
+TIMESTAMP=$(date '+%F_%H-%M-%S')
+> iperf_$TIMESTAMP.txt
+
+# clearing iptables counters
+ssh -i $RSA_KEY $USERNAME@$SERVER_IP "sudo iptables -Z IPERF_IN"
+sudo iptables -Z IPERF_OUT
+
+# printing iptables counters to the screen; they are supposed to be zeros
+# echo; sudo iptables -L IPERF_OUT -v -n -x
+# echo; ssh -i $RSA_KEY $USERNAME@$SERVER_IP "sudo iptables -L IPERF_IN -v -n -x"
+# echo
+
+# printing iptables counters to a temporary file; they are supposed to be zeros
+> iptables.tmp
+sudo iptables -L IPERF_OUT -v -n -x >> iptables.tmp
+ssh -i $RSA_KEY $USERNAME@$SERVER_IP "sudo iptables -L IPERF_IN -v -n -x" >> iptables.tmp
+
+printf "%s\n" "iPerf3 will run 3 flows of IMIX traffic for the duration of $TIME seconds:"
+printf "%s %'d bits/s\n" " 64-byte packets   (18 bytes of payload)   with configured bandwidth of" $BW_64_REF 
+printf "%s %'d bits/s\n" " 576-byte packets  (530 bytes of payload)  with configured bandwidth of" $BW_576_REF 
+printf "%s %'d bits/s\n" " 1350-byte packets (1304 bytes of payload) with configured bandwidth of" $BW_1350_REF
+
+printf "%s\n" "iPerf3 will run 3 flows of IMIX traffic for the duration of $TIME seconds:" >> iperf_$TIMESTAMP.txt
+printf "%s %'d bits/s\n" " 64-byte packets   (18 bytes of payload)   with configured bandwidth of" $BW_64_REF  >> iperf_$TIMESTAMP.txt
+printf "%s %'d bits/s\n" " 576-byte packets  (530 bytes of payload)  with configured bandwidth of" $BW_576_REF  >> iperf_$TIMESTAMP.txt
+printf "%s %'d bits/s\n" " 1350-byte packets (1304 bytes of payload) with configured bandwidth of" $BW_1350_REF >> iperf_$TIMESTAMP.txt
+
+echo "iperf3 -u -c $SERVER_IP -p 5001 -i 10 -t $TIME -l 18 -A 0 -T 64-byte_ip_payload -Z -P 8 -b $BW_64 -V &" > ./iperf_cpu0_results.tmp
+echo "iperf3 -u -c $SERVER_IP -p 5002 -i 10 -t $TIME -l 530 -A 2 -T 576-byte_ip_payload -Z -P 8 -b $BW_576 -V &" > ./iperf_cpu1_results.tmp
+echo "iperf3 -u -c $SERVER_IP -p 5003 -i 10 -t $TIME -l 1304 -A 4 -T 1350-byte_ip_payload -Z -P 8 -b $BW_1350 -V" > ./iperf_cpu2_results.tmp
+
+iperf3 -u -c $SERVER_IP -p 5001 -i 10 -t $TIME -l 18 -A 0 -T 64-byte_ip_payload -Z -P 8 -b $BW_64 -V >> ./iperf_cpu0_results.tmp &
+iperf3 -u -c $SERVER_IP -p 5002 -i 10 -t $TIME -l 530 -A 2 -T 576-byte_ip_payload -Z -P 8 -b $BW_576 -V >> ./iperf_cpu1_results.tmp &
+iperf3 -u -c $SERVER_IP -p 5003 -i 10 -t $TIME -l 1304 -A 4 -T 1350-byte_ip_payload -Z -P 8 -b $BW_1350 -V >> ./iperf_cpu2_results.tmp
+
+sleep 1
+echo 
+
+# appending the results of iPerf to a single file
+cat iperf_cpu*.tmp >> iperf_$TIMESTAMP.txt
+
+# printing iptables counters to the screen
+# echo; sudo iptables -L IPERF_OUT -v -n -x
+# echo; ssh -i $RSA_KEY $USERNAME@$SERVER_IP "sudo iptables -L IPERF_IN -v -n -x"
+# echo
+
+# printing iptables counters to separate files
+sudo iptables -L IPERF_OUT -v -n -x > iptables_out.tmp
+ssh -i $RSA_KEY $USERNAME@$SERVER_IP "sudo iptables -L IPERF_IN -v -n -x" > iptables_in.tmp
+
+cat iptables.tmp >> iperf_$TIMESTAMP.txt
+echo >> iperf_$TIMESTAMP.txt
+cat iptables_out.tmp >> iperf_$TIMESTAMP.txt
+echo >> iperf_$TIMESTAMP.txt
+cat iptables_in.tmp >> iperf_$TIMESTAMP.txt
+echo >> iperf_$TIMESTAMP.txt
+
+# FLOW_64_IN/OUT represents the number of packet received/sent
+FLOW_64_IN=$(cat iptables_in.tmp | grep 5001 | awk '{print $1}')
+FLOW_64_OUT=$(cat iptables_out.tmp | grep 5001 | awk '{print $1}')
+FLOW_576_IN=$(cat iptables_in.tmp | grep 5002 | awk '{print $1}')
+FLOW_576_OUT=$(cat iptables_out.tmp | grep 5002 | awk '{print $1}')
+FLOW_1350_IN=$(cat iptables_in.tmp | grep 5003 | awk '{print $1}')
+FLOW_1350_OUT=$(cat iptables_out.tmp | grep 5003 | awk '{print $1}')
+SUMMARY_IN=$(echo "$FLOW_64_IN + $FLOW_576_IN + $FLOW_1350_IN" | bc)
+SUMMARY_OUT=$(echo "$FLOW_64_OUT + $FLOW_576_OUT + $FLOW_1350_OUT" | bc)
+
+FLOW_64_LOST=$(echo "$FLOW_64_OUT - $FLOW_64_IN" | bc)
+FLOW_576_LOST=$(echo "$FLOW_576_OUT - $FLOW_576_IN" | bc)
+FLOW_1350_LOST=$(echo "$FLOW_1350_OUT - $FLOW_1350_IN" | bc)
+SUMMARY_LOST=$(echo "$FLOW_64_LOST + $FLOW_576_LOST + $FLOW_1350_LOST" | bc)
+
+FLOW_64_DROP_RATE=$( echo "$FLOW_64_LOST * 100 / $FLOW_64_OUT" | bc )
+FLOW_576_DROP_RATE=$( echo "$FLOW_576_LOST * 100 / $FLOW_576_OUT" | bc )
+FLOW_1350_DROP_RATE=$( echo "$FLOW_1350_LOST * 100 / $FLOW_1350_OUT" | bc )
+SUMMARY_DROP_RATE=$(printf "%0.2f" $(echo "$SUMMARY_LOST * 100 / $SUMMARY_OUT" | bc) )
+
+# Bandwidth is counted with the following formula:
+#  IFG + Preamble + Ethernet + IP + UDP + payload + CRS
+#  12  +    8     +    14    + 20 +  8  + payload +   4 = 66 + payload
+
+# 64-bytes:   payload is 18, which results in 84 bytes per frame
+# 576-bytes:  payload is 530, which results in 596 bytes per frame
+# 1350-bytes: payload is 1304, which results in 1370 bytes per frame
+# Next, bytes must be converted to megabits, which results in
+# multiplication by 8 and division by 1 000 000, or multiplication by 0.000008
+
+FLOW_64_BW_IN=$( echo "scale=3; ($FLOW_64_IN * 84 * 0.000008)/($TIME)" | bc ) 
+FLOW_576_BW_IN=$( echo "scale=3; ($FLOW_576_IN * 596 * 0.000008)/($TIME)" | bc ) 
+FLOW_1350_BW_IN=$( echo "scale=3; ($FLOW_1350_IN * 1304 * 0.000008)/($TIME)" | bc ) 
+SUMMARY_BW_IN=$(echo "$FLOW_64_BW_IN + $FLOW_576_BW_IN + $FLOW_1350_BW_IN" | bc )
+
+FLOW_64_BW_OUT=$( echo "scale=3; ($FLOW_64_OUT * 84 * 0.000008)/($TIME)" | bc ) 
+FLOW_576_BW_OUT=$( echo "scale=3; ($FLOW_576_OUT * 596 * 0.000008)/($TIME)" | bc ) 
+FLOW_1350_BW_OUT=$( echo "scale=3; ($FLOW_1350_OUT * 1304 * 0.000008)/($TIME)" | bc ) 
+SUMMARY_BW_OUT=$(echo "$FLOW_64_BW_OUT + $FLOW_576_BW_OUT + $FLOW_1350_BW_OUT" | bc )
+
+echo "iPerf3 was running for "$TIME" seconds with bandwidth limit set to "$2"Mb/s."
+echo
+printf "%-10s%15s%15s%11s%12s%14s%14s\n" "Data" "Sent" "Received" "Lost" "Drop" "Bandwidth" "Bandwidth"
+printf "%-10s%15s%15s%11s%12s%14s%14s\n" "flow" "packets" "packets" "packets" "rate" "(sender)" "(receiver)"
+
+printf "%-10s%15s%15s%11d%11.2f%%%10s%s%10s%s\n" "64-byte" $FLOW_64_OUT $FLOW_64_IN $FLOW_64_LOST $FLOW_64_DROP_RATE $FLOW_64_BW_OUT "Mb/s" $FLOW_64_BW_IN "Mb/s"
+printf "%-10s%15s%15s%11d%11.2f%%%10s%s%10s%s\n" "576-byte" $FLOW_576_OUT $FLOW_576_IN $FLOW_576_LOST $FLOW_576_DROP_RATE $FLOW_576_BW_OUT "Mb/s" $FLOW_576_BW_IN "Mb/s"
+printf "%-10s%15s%15s%11d%11.2f%%%10s%s%10s%s\n" "1350-byte" $FLOW_1350_OUT $FLOW_1350_IN $FLOW_1350_LOST $FLOW_1350_DROP_RATE $FLOW_1350_BW_OUT "Mb/s" $FLOW_1350_BW_IN "Mb/s"
+echo
+printf "%-10s%15s%15s%11d%11.2f%%%10s%s%10s%s\n" "Summary" $SUMMARY_OUT $SUMMARY_IN $SUMMARY_LOST $SUMMARY_DROP_RATE $SUMMARY_BW_OUT "Mb/s" $SUMMARY_BW_IN "Mb/s"
+
+echo >> iperf_$TIMESTAMP.txt
+echo "iPerf3 was running for "$TIME" seconds with bandwidth limit set to "$2"Mb/s." >> iperf_$TIMESTAMP.txt
+echo >> iperf_$TIMESTAMP.txt
+printf "%-10s%15s%15s%11s%12s%14s%14s\n" "Flow" "Sent" "Received" "Lost" "Drop" "Bandwidth" "Bandwidth" >> iperf_$TIMESTAMP.txt
+printf "%-10s%15s%15s%11s%12s%14s%14s\n" "    " "packets" "packets" "packets" "rate" "(sender)" "(receiver)" >> iperf_$TIMESTAMP.txt
+printf "%-10s%15s%15s%11d%11.2f%%%10s%s%10s%s\n" "64-byte" $FLOW_64_OUT $FLOW_64_IN $FLOW_64_LOST $FLOW_64_DROP_RATE $FLOW_64_BW_OUT "Mb/s" $FLOW_64_BW_IN "Mb/s" >> iperf_$TIMESTAMP.txt
+printf "%-10s%15s%15s%11d%11.2f%%%10s%s%10s%s\n" "576-byte" $FLOW_576_OUT $FLOW_576_IN $FLOW_576_LOST $FLOW_576_DROP_RATE $FLOW_576_BW_OUT "Mb/s" $FLOW_576_BW_IN "Mb/s" >> iperf_$TIMESTAMP.txt
+printf "%-10s%15s%15s%11d%11.2f%%%10s%s%10s%s\n" "1350-byte" $FLOW_1350_OUT $FLOW_1350_IN $FLOW_1350_LOST $FLOW_1350_DROP_RATE $FLOW_1350_BW_OUT "Mb/s" $FLOW_1350_BW_IN "Mb/s" >> iperf_$TIMESTAMP.txt
+echo >> iperf_$TIMESTAMP.txt
+printf "%-10s%15s%15s%11d%11.2f%%%10s%s%10s%s\n" "Summary" $SUMMARY_OUT $SUMMARY_IN $SUMMARY_LOST $SUMMARY_DROP_RATE $SUMMARY_BW_OUT "Mb/s" $SUMMARY_BW_IN "Mb/s" >> iperf_$TIMESTAMP.txt
+
+rm -f *.tmp
+
+exit
